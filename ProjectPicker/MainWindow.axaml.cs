5eda56cfd9181ecaa52e172133ac34d9ef148750
smalltownmod <cthulhukultist@gmail.com>
2024-01-24 11:46:49 +0100

    project create

---
+++ ProjectPicker/MainWindow.axaml.cs
@@ -0,0 +1,9 @@
+using Avalonia.Controls;
+
+namespace ProjectPicker {
+	public partial class MainWindow : Window {
+		public MainWindow() {
+			InitializeComponent();
+		}
+	}
+}
---
+++ Tests.ProjectPicker/UnitTest1.cs
@@ -0,0 +1,8 @@
+namespace Tests.ProjectPicker {
+	public class UnitTest1 {
+		[Fact]
+		public void Test1() {
+
+		}
+	}
+}
