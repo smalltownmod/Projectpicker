35b040c2915d9c0e539ac37b784d913bd5329a5c
smalltownmod <cthulhukultist@gmail.com>
2024-01-24 13:56:20 +0100

    first UnitTests

---
+++ Tests.ProjectPicker/CreateTest.cs
@@ -0,0 +1,35 @@
+
+using ProjectPicker;
+using ProjectPicker.models;
+
+namespace Tests.ProjectPicker {
+	public class CreateTest {
+		string Path = "E://src";
+		string proj = "CreateTest";
+		[Fact]
+		public void Create() {
+		string wordir = $"{Path}/{proj}";
+			if (Directory.Exists(wordir)) Directory.Delete(wordir, true);
+			Directory.CreateDirectory(wordir);
+		}
+		[Fact]
+		public void SlnCreate() {
+			//Environment.CurrentDirectory= Path + "/" + proj;
+			var Exitcode = ProcInvoker.Run("dotnet", $"new sln -n {proj} -o {Path}/{proj}");
+			Assert.True(Exitcode == 0);
+		}
+
+		[Fact]
+		public void createAva() {
+			createProject("Avalonia App", proj, proj);
+		}
+
+		public void createProject(string type, string name, string dir) {
+		var Exitcode =	ProcInvoker.Run("dotnet", $" new {type.Split(' ').First().ToLower()}.{type.Split(' ').Last().ToLower()} -n {name} -o {Path}/{proj}/{dir} ");
+			Exitcode += ProcInvoker.Run("dotnet", $" sln {Path}/{proj}/{proj}.sln add {Path}/{proj}/{dir}/{name}.csproj");
+			var rep = $"{ Path }/{ proj}/{ dir}/{ name}.csproj";
+			File.WriteAllText(rep, File.ReadAllText(rep).Replace("net8.0", "net7.0"));
+			Assert.True(Exitcode == 0);
+		}
+	}
+}
--- Tests.ProjectPicker/UnitTest1.cs
+++
@@ -1,8 +0,0 @@
-namespace Tests.ProjectPicker {
-	public class UnitTest1 {
-		[Fact]
-		public void Test1() {
-
-		}
-	}
-}
